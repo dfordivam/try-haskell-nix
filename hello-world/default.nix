3e52d5a7f720b77e2f9f590c63ed3ab89f1d290a
Divam <dfordivam@gmail.com>
2021-01-06 21:33:59 +0900

    pin haskell.nix and fix

--- hello-world/default.nix
+++ hello-world/default.nix
@@ -1,10 +1,10 @@
 { # Fetch the latest haskell.nix and import its default.nix
-  haskellNix ? import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz") {}
+  haskellNix ? import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/e02d5a8ca42d10577a4e78e64e4e8c52afeaacd4.tar.gz") {}
 
 # haskell.nix provides access to the nixpkgs pins which are used by our CI,
 # hence you will be more likely to get cache hits when using these.
 # But you can also just use your own, e.g. '<nixpkgs>'.
-, nixpkgsSrc ? haskellNix.sources.nixpkgs-2003
+, nixpkgsSrc ? haskellNix.sources.nixpkgs-2009
 
 # haskell.nix provides some arguments to be passed to nixpkgs, including some
 # patches and also the haskell.nix functionality itself as an overlay.
@@ -15,7 +15,7 @@
 }: pkgs.haskell-nix.project {
   # 'cleanGit' cleans a source directory based on the files known by git
   src = pkgs.haskell-nix.haskellLib.cleanGit {
-    name = "haskell-nix-project";
+    name = "hello-world";
     src = ./.;
   };
   # Specify the GHC version to use.
