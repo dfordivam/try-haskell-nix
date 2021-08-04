5e26d59b21215f3137720c176c0a8878e4f11ec6
Divam <dfordivam@gmail.com>
2021-08-04 18:38:14 +0900

    Add a lens TH, and reflex-dom code

--- hello-world/MyLib.hs
+++ hello-world/MyLib.hs
@@ -1,4 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 module MyLib (someFunc) where
 
+import Reflex.Dom
+import Control.Lens
+
+data MyData = MyData
+  { _myData_foo :: Int
+  , _myData_bar :: Bool
+  }
+  deriving (Show)
+
+makeLenses ''MyData
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  putStrLn "someFunc"
+  let s = MyData 1 True & myData_foo %~ (+ 1)
+  print s
+
+  mainWidget $ do
+    el "h2" $ text "Working reflex"
+    display =<< count =<< button "test"
